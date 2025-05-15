a65f99568302e3d193f9930977a99810c5a66448
rafael-dumont <rafael_dumont@hotmail.com>
2025-05-15 05:13:17 -0300

    Add files via upload

---
+++ scripts/scroll-horizontal.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".slide");
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontal-scroll",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () => "+=" + document.querySelector(".slider").offsetWidth
+  }
+});
